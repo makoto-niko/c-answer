ef6f9cf45037d1e5ad8efad4b46be3daf4d0d443
makoto-niko <mmana19970613@gmail.com>
2024-03-26 07:49:22 +0000

    .env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,16 +74,16 @@ Rails.application.configure do
   config.hosts.clear
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
-  config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    port:                 587,
     address:              'smtp.gmail.com',
-    port:                  587,
-    domain:               'gmail.com',
-    user_name:            ENV['MAIL_ADDRESS'],
-    password:             ENV['MAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto:  true
+    domain:               'smtp.gmail.com',
+    user_name:            ENV['WELCOME_MAILER_ADDRESS'],
+    password:             ENV['WELCOME_MAILER_PASSWORD'],
+    authentication:       'login',
+    enable_starttls_auto: true
   }
-  
 end
+  
